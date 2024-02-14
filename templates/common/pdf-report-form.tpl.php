ad0db83f9d4cbd912a29877b9138262f9e784373
Vishnu K P <vishnu.p@ennexa.com>
2024-02-14 10:41:40 +0530

    style: ashtakavarga removed from pdf report and change input type to hidden for planet and hide ashtakavarga

--- templates/common/pdf-report-form.tpl.php
+++ templates/common/pdf-report-form.tpl.php
@@ -57,30 +57,8 @@ use Prokerala\Api\Astrology\Result\Planet;
         </select>
     </div>
 </div>
-<div class="form-group row">
-    <label class="col-sm-3 col-md-4 col-form-label  text-md-right text-xs-left ">Hide Ashtakavarga:</label>
-    <div class="col-sm-9 col-md-6 ">
-        <select name="hide_ashtakavarga" class="form-control form-control-lg rounded-1">
-            <option value="1" >true</option>
-            <option value="0" selected>false</option>
-        </select>
-    </div>
-</div>
-
-<div class="form-group row">
-    <label class="col-sm-3 col-md-4 col-form-label  text-md-right text-xs-left ">Chart:</label>
-    <div class="col-sm-9 col-md-6 ">
-        <select name="planet" class="form-control form-control-lg rounded-1">
-
-            <?php foreach (Planet::PLANET_LIST as $planet_id => $planet): ?>
-
-                <option value="<?=$planet_id?>" ><?=$planet?></option>
-
-              <?php endforeach; ?>
-
-        </select>
-    </div>
-</div>
+<input type="hidden" name="hide_ashtakavarga" value="0">
+<input type="hidden" name="planet" value="0">
 
 <div class="form-group row">
     <label class="col-sm-3 col-md-4 col-form-label  text-md-right text-xs-left">Date:<sup class="text-danger">*</sup></label>
